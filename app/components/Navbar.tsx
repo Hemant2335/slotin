ce77f81c8a857130220a2cdb3843f3308d353a99
Hemant2335 <knrt73373@gmail.com>
2024-06-02 23:25:28 -0400

    Made_Some_basic_Homepage

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,28 @@
+import React from 'react'
+import { FiHome } from 'react-icons/fi';
+import { FiUser } from 'react-icons/fi';
+import { FiBriefcase } from 'react-icons/fi';
+import { FiBox } from 'react-icons/fi';
+import { FiGrid } from 'react-icons/fi';
+import { FiCompass } from 'react-icons/fi';
+import { FiMessageSquare } from 'react-icons/fi';
 
-export default function Navbar(){
-    return (
-        <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-            </ul>
-        </nav>
-    )
+const Navbar = () => {
+
+
+  return (
+    <div className='w-full mt-[4vh] h-[5vh] flex items-center justify-center '>
+        <div className='bg-[#1a1a1a] w-fit h-fit shadow-3xl p-3 title rounded-xl fixed z-10'>
+            <nav>
+                <ul className='flex items-center font-bold gap-[3vh] text-md'>
+                    <li className='hover:text-[#097969] cursor-pointer transition-transform nav-home ' >Home</li>
+                    <li className='hover:text-[#097969] cursor-pointer transition-transform nav-About' >Contact</li>
+                    <li className='hover:text-white cursor-pointer transition-transform nav-msg bg-[#097969] py-1 px-2 rounded-md' >Login</li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+  )
 }
+
+export default Navbar
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,40 @@
 import Image from "next/image";
+import background from "./assets/bg2.jpeg";
+import poster from "./assets/poster.jpg";
 
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Next.js!</h1>
-      <Image
-        src="/assets/background.jpeg"
-        alt="Vercel Logo"
-        width={72}
-        height={16}
-      />
+      <div className="w-full h-screen absolute z-10 py-[8vh] px-[3vw]">
+        <div className="p-4 my-10 flex">
+          <div className="w-full h-fit bg-cover md:flex justify-between ">
+            <div className="items-center mt-10">
+              <h1 className="text-7xl font-poppins font-bold head">The Best</h1>
+              <h1 className="text-7xl font-poppins font-bold head">
+                Sloting Services
+              </h1>
+              <h1 className="text-7xl font-poppins font-bold text-[#097969] head">
+                in Your City
+              </h1>
+              <p className="text-xl font-poppins font-medium mt-10 head">
+                Slotin assures quality and Also to mange everyones time
+                efficiently.
+              </p>
+              <button
+                className="shadow-3xl text-lg mt-[5vh] font-medium font-poppins px-4 py-2 bg-[#097969] rounded-md hover:bg-red-400 hover:text-black transition-transform"
+                id="btn"
+              >
+                book now
+              </button>
+            </div>
+          </div>
+          <Image
+            src={poster}
+            alt="poster"
+            className="hidden  md:flex max-w-[45vw] rounded-lg shadow-xl"
+          />
+        </div>
+      </div>
     </div>
   );
 }
